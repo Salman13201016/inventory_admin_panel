29a239c047a96150e7421ddc3e30335fa2258dc0
Salman13201016 <salmanmdsultan@gmail.com>
2021-11-10 01:36:25 +0600

    Added my project

---
+++ admin/controller/index.php
@@ -0,0 +1,71 @@
+<?php include_once '../model/db_config.php'; ?>
+
+<?php 
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        $cat_type_name =trim($_POST['cat_type_name']);
+        $cat_type_code =trim($_POST['cat_type_code']);
+        $sql = "INSERT INTO category_types (cat_type_name, cat_type_code) VALUES (?, ?)";
+        $sql_statment = mysqli_prepare($link,$sql);
+        if ($sql_statment){
+            // print_r('ssssss');
+            mysqli_stmt_bind_param($sql_statment, "ss", $var1,$var2);
+            $var1=$cat_type_name;
+            $var2 = $cat_type_code;
+            $execute = mysqli_stmt_execute($sql_statment);
+            if($execute){
+                header("location: index.php");
+            }
+            else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+    }
+?>
+<!DOCTYPE html>
+<html>
+
+<?php include '../view/layout/header.php'; ?>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        
+        <?php include '../view/layout/side_navbar.php'; ?>
+        <!-- Page Content Holder -->
+        <div id="content">
+            <?php include '../view/layout/content_menu.php'; ?>
+            <div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="mb-3">
+                <h3>Add Category Types</h3>
+            </div>
+            <form accept="" class="shadow p-4" action="index.php" method="POST">                  
+                <div class="mb-3">
+                    <label for="cat_type_name">Category Type Name</label>
+                    <input type="text" class="form-control" name="cat_type_name" id="cat_type_name" placeholder="Category Type Name">
+                </div>
+
+                <div class="mb-3">
+                    <label for="cat_type_code">Category Type Code</label>
+                    <input type="text" class="form-control" name="cat_type_code" id="cat_type_code" placeholder="Category Type Code">
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Add Category Types</button>
+                </div>
+                
+            </form>
+        </div>
+    </div>
+</div>
+            
+            
+        </div>
+    </div>
+
+    <?php include '../view/layout/js_lib.php'; ?>
+</body>
+
+</html>
---
+++ admin/view/layout/side_navbar.php
@@ -0,0 +1,44 @@
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>Admin Panel</h3>
+    </div>
+
+    <ul class="list-unstyled components">
+        <!-- <p>Dummy Heading</p> -->
+        <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Category Types</a>
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="index.php">Add Category Types</a>
+                </li>
+                <li>
+                    <a href="#">Category Types List</a>
+                </li>
+                <!-- <li>
+                    <a href="#">Home 3</a>
+                </li> -->
+            </ul>
+        </li>
+        <li>
+            <a href="#">About</a>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a href="#">Page 1</a>
+                </li>
+                <li>
+                    <a href="#">Page 2</a>
+                </li>
+                <li>
+                    <a href="#">Page 3</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Portfolio</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+    </ul>
+</nav>
