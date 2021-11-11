8450dce42335a4ee83ab4df3b3b2d738156bef11
Salman13201016 <salmanmdsultan@gmail.com>
2021-11-12 00:11:30 +0600

    Empty Form Validation

--- admin/controller/index.php
+++ admin/controller/index.php
@@ -1,25 +1,49 @@
 <?php include_once '../model/db_config.php'; ?>
 
-<?php 
+<?php
+    $error1=$error2=$error3=$success="";
+    $cat_type_name=$cat_type_code="";
     if($_SERVER['REQUEST_METHOD']=="POST"){
         $cat_type_name =trim($_POST['cat_type_name']);
         $cat_type_code =trim($_POST['cat_type_code']);
-        $sql = "INSERT INTO category_types (cat_type_name, cat_type_code) VALUES (?, ?)";
-        $sql_statment = mysqli_prepare($link,$sql);
-        if ($sql_statment){
-            // print_r('ssssss');
-            mysqli_stmt_bind_param($sql_statment, "ss", $var1,$var2);
-            $var1=$cat_type_name;
-            $var2 = $cat_type_code;
-            $execute = mysqli_stmt_execute($sql_statment);
-            if($execute){
-                header("location: index.php");
+        if(empty($cat_type_name) || empty($cat_type_code)){
+            if(empty($cat_type_name) && empty($cat_type_code)){
+                $error1 = "Please fill up both forms";
             }
-            else{
-                echo "Oops! Something went wrong. Please try again later.";
+            else if (empty($cat_type_name)){
+
+                $error2 = "Please Insert Category Type Name";
+                echo $error2;
+            }
+            else if (empty($cat_type_code)){
+                $error3 = "Please Insert Category Type Code";
+            }
+
+            
+        }
+        else{
+            // echo "1";
+            $sql = "INSERT INTO category_types (cat_type_name, cat_type_code) VALUES (?, ?)";
+            $sql_statment = mysqli_prepare($link,$sql);
+            if ($sql_statment){
+                // print_r('ssssss');
+                mysqli_stmt_bind_param($sql_statment, "ss", $var1,$var2);
+                $var1=$cat_type_name;
+                $var2 = $cat_type_code;
+                $execute = mysqli_stmt_execute($sql_statment);
+                if($execute){
+                    $success = "Successfully Inserted";
+                    //header("location: index.php");
+
+                }
+                else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
             }
         }
+        
     }
+        
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,16 +64,37 @@
         <div class="col-md-6 offset-md-3">
             <div class="mb-3">
                 <h3>Add Category Types</h3>
+                <?php 
+                    if(!empty($success)){
+                        echo '<span style="color:green;">'.$success.'</span>';
+                    }
+                    else{
+                        echo '<span style="color:red;">'.$error1.'</span>';
+                    }
+                ?>
+                
             </div>
-            <form accept="" class="shadow p-4" action="index.php" method="POST">                  
+            <form class="shadow p-4" method="POST" action="index.php">                  
                 <div class="mb-3">
                     <label for="cat_type_name">Category Type Name</label>
-                    <input type="text" class="form-control" name="cat_type_name" id="cat_type_name" placeholder="Category Type Name">
+                    <input value="<?php echo $cat_type_name;?>" type="text" class="form-control" name="cat_type_name"  id="cat_type_name" placeholder="Category Type Name">
+                    <?php 
+                        if(!empty($error2)){
+
+                            echo '<span style="color:red;">'.$error2.'</span>';
+                        }
+                    ?>
                 </div>
 
                 <div class="mb-3">
                     <label for="cat_type_code">Category Type Code</label>
-                    <input type="text" class="form-control" name="cat_type_code" id="cat_type_code" placeholder="Category Type Code">
+                    <input value="<?php echo $cat_type_code;?>" type="text" class="form-control" name="cat_type_code" id="cat_type_code" placeholder="Category Type Code">
+                    <?php 
+                        if(!empty($error3)){
+
+                            echo '<span style="color:red;">'.$error3.'</span>';
+                        }
+                    ?>
                 </div>
 
                 <div class="mb-3">
--- admin/model/db_config.php
+++ admin/model/db_config.php
@@ -12,7 +12,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-// // else{
-// //     echo "success";
-// // }
+// else{
+//     echo "success";
+// }
 ?>
